b7a66df2d7af202b630a55daca4fd03a6cf367df
vasikarla <vasikarla_raj@yahoo.com>
2014-09-07 18:09:01 -0700

    Create presetup.sh
    presetup.sh

---
+++ presetup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+defaults write com.apple.Safari CustomUserAgent "\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.74.9 (KHTML, like Gecko) IntuitEmbedded (QB-Cloud/1.0/AppStore)\""
